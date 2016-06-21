032902751b97d6b8af1e32d150d524e37d29f1bf
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-06-21 20:55:31 +0200

    gruntify the project

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['src/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.es5.js']
+                }
+            }
+        },
+        jshint: {
+            // define the files to lint
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+            options: {
+                // more options here if you want to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.es5.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-babel');
+
+    // the default task can be run just by typing "grunt" on the command line
+    grunt.registerTask('default', ['jshint', 'concat', 'babel', 'uglify']);
+};
