93eaa60e219291358d63d34deba2493c7fece4c6
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-12-20 15:11:26 +0100

    Fixed exporting as jQuery function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-export {Heatmap} from "./heatmap/heatmap";
+import {Heatmap} from "./heatmap/heatmap";
 
-// TODO: How to export jQuery function here?
 $.fn.heatmap = function(data, options) {
     return new Heatmap(this.get(0), data, options);
 };
