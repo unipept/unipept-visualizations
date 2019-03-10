569f75a7f270d8bf8a3d811d6c454f88411daa7e
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-03-10 11:26:34 +0100

    Implemented MOLO heuristics for reordering dendrogram

--- src/cluster/treeNode.ts
+++ src/cluster/treeNode.ts
@@ -1,6 +1,6 @@
 export default class TreeNode<T> {
-    public readonly leftChild: TreeNode<T> | null;
-    public readonly rightChild: TreeNode<T> | null;
+    private _leftChild: TreeNode<T> | null;
+    private _rightChild: TreeNode<T> | null;
     public readonly values: T[];
     public readonly height: number;
 
@@ -10,11 +10,28 @@ export default class TreeNode<T> {
     public readonly id: string;
 
     constructor(leftChild: TreeNode<T> | null, rightChild: TreeNode<T> | null, values: T[], height: number) {
-        this.leftChild = leftChild;
-        this.rightChild = rightChild;
+        this._leftChild = leftChild;
+        this._rightChild = rightChild;
         this.values = values;
         this.height = height;
         this.id = TreeNode.currentID.toString();
         TreeNode.currentID++;
+    }
+
+
+    get leftChild(): TreeNode<T> | null {
+        return this._leftChild;
+    }
+
+    set leftChild(value: TreeNode<T> | null) {
+        this._leftChild = value;
+    }
+
+    get rightChild(): TreeNode<T> | null {
+        return this._rightChild;
+    }
+
+    set rightChild(value: TreeNode<T> | null) {
+        this._rightChild = value;
     }
 }
