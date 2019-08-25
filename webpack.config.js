47359138f25b8914a7b355893d250e15d71bc8d5
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-08-25 17:28:07 +0200

    set up new build system

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.ts'),
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          'babel-loader',
+          'ts-loader'
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Heatmap',
+    libraryTarget: 'umd'
+  },
+  mode: 'development'
+};
