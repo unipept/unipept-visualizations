db9081015534b138b0fb0c64708d0a3069710cd9
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-08-25 21:05:10 +0200

    add ci

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,10 @@
 import { rgb } from "d3-color";
 
+/**
+ * Calculates a numeric hash value of a string
+ * 
+ * @param s string to calculate a hash from
+ */
 export function stringHash(s: string): number {
     return s.split("").reduce(function (a, b) {
         const c = ((a << 5) - a) + b.charCodeAt(0);
@@ -7,16 +12,20 @@ export function stringHash(s: string): number {
     }, 0);
 }
 
-/*
+/**
  * Returns the brightness of an rgb-color
  * from: http:// www.w3.org/WAI/ER/WD-AERT/#color-contrast
+ * 
+ * @param rgb rgb color to calculate the brightness from
  */
 export function brightness(rgb: d3.RGBColor): number {
     return rgb.r * 0.299 + rgb.g * 0.587 + rgb.b * 0.114;
 }
 
-/*
+/**
  * Returns the readable text color based on the brightness of a given backgroud color
+ * 
+ * @param color hex value of a color
  */
 export function getReadableColorFor(color: string): string {
     let textColor = "#000";
