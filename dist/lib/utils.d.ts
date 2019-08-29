b7463cf06d3bd6819225c7c4a03782df81e138f7
James Collier <james.collier@vib.be>
2019-08-29 14:06:57 +0200

    Remove dist/ from the git index
    Transpiled output should not be in git

--- dist/lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function stringHash(s: string): number;
-export declare function brightness(rgb: d3.RGBColor): number;
-export declare function getReadableColorFor(color: string): string;
