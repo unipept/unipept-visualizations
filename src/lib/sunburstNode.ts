4ef38f0ed7d9928f474967f99d8afd8f2b31ee1c
James Collier <james.collier@vib.be>
2019-08-29 10:23:29 +0200

    Script re-implementation of src-old/sunburst/sunburstNode.js

---
+++ src/lib/sunburstNode.ts
@@ -0,0 +1,19 @@
+/**
+ * //TODO: docs
+ */
+import { BasicNode, emptyBasicNode } from "./basicNode";
+import { Node } from "./node";
+
+export class SunburstNode extends Node {
+    constructor(node: BasicNode = emptyBasicNode()) {
+        super(node);
+    }
+
+    static new(node: BasicNode = emptyBasicNode()): SunburstNode {
+        return new SunburstNode(node);
+    }
+
+    static createNodes(node: BasicNode): SunburstNode {
+        return Node.createNode(node, SunburstNode.new);
+    }
+}
