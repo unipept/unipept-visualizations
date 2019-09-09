39164bcf3cb0383fcd5b88117eb021a0960cd11b
James Collier <james.collier@vib.be>
2019-09-09 10:43:22 +0200

    Clean up Utils and add more tests
    Also implement new function: generateId()

--- src/lib/__tests__/utils.test.ts
+++ src/lib/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { stringHash } from "../utils";
+import { stringHash, generateId } from "../utils";
 
 test("hash for a string to be stable", () => {
   const text: string = "this is a string";
@@ -6,3 +6,12 @@ test("hash for a string to be stable", () => {
     .toMatchInlineSnapshot("-1853110172");
 });
 
+test("Default ID length", () => {
+  const v: string = generateId();
+  expect(v.length).toEqual(10);
+});
+
+test("Specific ID length", () => {
+  const v: string = generateId(6);
+  expect(v.length).toEqual(6);
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,13 +5,34 @@
  * 
  * @param s string to calculate a hash from
  */
-export function stringHash(s: string): number {
-  return s.split("").reduce(function(a, b) {
-    const c = ((a << 5) - a) + b.charCodeAt(0);
-    return c & c;
-  }, 0);
-}
+const stringHash: (s: string) => number
+  = (s: string): number => {
+    return s.split("").reduce((a, b) => {
+      const c = ((a << 5) - a) + b.charCodeAt(0);
 
-export function functor(v: Function | any): Function {
-  return typeof v === "function" ? v : function() { return v; };
-}
+      return c & c;
+    }, 0);
+  };
+
+/**
+ * If the argument is not a function, wrap it in a function that returns that value
+ */
+const wrapFunction: (v: (() => any) | any) => (() => any)
+  = (v: (() => any) | any): (() => any)  =>
+  (typeof v === "function" ? v : ((): any => v));
+
+/**
+ * Generate an element identifier
+ * Math.random = [0, 1)
+ * Number.toString([radix]) 26 letters + 10 digits
+ */
+const generateId: (length?: number) => string
+  = (length?: number): string => {
+    const l: number = length || 10;
+
+    return `${Math.random().toString(36)}`.substr(2, l);
+  };
+
+
+
+export { stringHash, wrapFunction, generateId };
