83d5569e015eb9082b30d12fa797ff95dada0430
James Collier <james.collier@vib.be>
2019-11-04 15:46:12 +0100

    Initial working import of the heatmap.
    Just the visualisation working. No labels yet.

--- src/lib/heatmap/settings.ts
+++ src/lib/heatmap/settings.ts
@@ -1,45 +1,59 @@
 import * as R from "ramda";
 
-import { Settings } from "../settings";
+import { BasicNode } from "../basicNode";
 import { Node } from "../node";
+import { Settings } from "../settings";
+
+import { HeatmapNode } from "./node";
 
 
 export class HeatmapSettings extends Settings {
   public static readonly DEFAULT_DURATION: number = 2000;
 
-    /***** VALUES *****/
-    // Amount of pixels that are allowed to be occupied by the labels of the rows.
-    textWidth: number = 100;
+  /***** VALUES *****/
+  // Amount of pixels that are allowed to be occupied by the labels of the rows.
+  public readonly textWidth: number = 100;
 
-    // Amount of pixels that are allowed to be occupied by the labels of the columns.
-    textHeight: number = 100;
+  // Amount of pixels that are allowed to be occupied by the labels of the columns.
+  public readonly textHeight: number = 100;
 
-    // The maximum dimension of one square.
-    maximumSquareWidth: number = 50;
+  // The maximum dimension of one square.
+  public readonly maximumSquareWidth: number = 50;
 
-    // Space between the squares in the grid (0 for no padding)
-    squarePadding: number = 2;
+  // Space between the squares in the grid (0 for no padding)
+  public readonly squarePadding: number = 2;
 
-    // Space between the visualization grid itself and rendering the labels (in pixels). This space is applied to both
-    // the rows and columns labels.
-    visualizationTextPadding = 5;
+  // Space between the visualization grid itself and rendering the labels (in pixels). This space is applied to both
+  // the rows and columns labels.
+  public readonly visualizationTextPadding: number = 5;
 
-    // Size of text used in the visualization (for row and column labels)
-    fontSize: number = 12;
+  // Size of text used in the visualization (for row and column labels)
+  public readonly fontSize: number = 12;
 
-    className = 'heatmap';
+  public readonly className: string = "heatmap";
+
+  public readonly colorScale: string[] = ["#EEEEEE", "#1565C0"];
 
   // Total speed of the reordering animations used in this visualization, should be given in milliseconds (ms).
-  duration: number = HeatmapSettings.DEFAULT_DURATION;
+  public readonly duration: number = HeatmapSettings.DEFAULT_DURATION;
 
   /***** FUNCTIONS *****/
 
-  dataAccessor: R.Lens = R.lensProp("value");
+  public readonly dataAccessor: (data: Node) => number
+    = (data: Node): number => (data as HeatmapNode).value
+
+  public readonly dataModifier: (value: number, data: Node) => HeatmapNode =
+    (value: number, data: Node): HeatmapNode =>
+    new HeatmapNode({...(data as HeatmapNode), value} as unknown as BasicNode)
 
   // Text that's displayed inside a tooltip.
   // This is equal to the current cell's value by default.
-  getTooltipText: (data: Node) => string
-    = (data: Node) => `score: ${(R.view(this.dataAccessor, data) as number * 100).toFixed(2)}%`;
+  public readonly getTooltipText: (data: Node) => string
+    = (data: Node) => {
+      const lens: R.Lens = R.lens(this.dataAccessor, this.dataModifier);
+
+      return `score: ${(R.view(lens, data) as number * 100).toFixed(2)}%`;
+    }
 
   public constructor(settings?: object) {
     super();
