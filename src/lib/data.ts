ddbba69f789028aa7c592c46765ec2331176b2c5
James Collier <james.collier@vib.be>
2020-06-30 17:06:40 +0200

    Add more documentation

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -78,12 +78,31 @@ const ancestorOf: (check: HierarchyNode<Node>,
 
 
 /**
- * This dataframe is indexed by row
+ * A rudimentary DataFrame (a la R and Python Pandas)
+ * This dataframe is column major-order. It is meant to
+ * be used internally by Unipept Visualisations, implementation
+ * details such as access through Ramda Lenses should not
+ * be revealed to the user.
+ *
+ * A dataframe is a mapping of column names to [[Series]].
+ * DataFrame<number> =>
+ * { "column A": Series<number>,
+ *   "column B": Series<number>
+ * }
+ * The `index` is an ordered list of the column labels.
+ * Each column (Series) need not be the same size, but
+ * shared series (row) labels will form a row.
  */
 class DataFrame<T> {
   public readonly data: { [k: string]: Series<T> } = {};
   public readonly index: string[];
 
+  /**
+   * Build a dataframe from a list of series
+   *
+   * @param data The data to be contained in the frame
+   * @param index (optional) column index
+   */
   public constructor(data: Array<Series<T>>, index?: string[]) {
     const realIndex: string[] = index ? index : data.map((_, i) => `${i}`);
 
@@ -91,10 +110,23 @@ class DataFrame<T> {
     this.index = realIndex;
   }
 
+  /**
+   * Access a value in the dataframe by row and column label
+   *
+   * @param row The label of the row to access
+   * @param column The label of the column to access
+   * @return The value being accessed
+   */
   public at(row: string, column: string): T {
     return this.data[column].at(row);
   }
 
+  /**
+   * Slice the Dataframe by a single column
+   *
+   * @param label The column label to slice
+   * @return the colunm data
+   */
   public column(label: string): Series<T> {
     if (this.index.includes(label)) {
       return this.data[label];
@@ -105,18 +137,37 @@ class DataFrame<T> {
     return new Series(new Array(size), this.rows());
   }
 
+  /**
+   * Return all column labels
+   */
   public columns(): string[] {
     return this.index;
   }
 
+  /**
+   * Access a value in the dataframe by row and column index
+   *
+   * @param row The index of the row to access
+   * @param column The index of the column to access
+   * @return The value being accessed
+   */
   public iat(row: number, column: number): T {
     return this.data[this.index[column]].iat(row);
   }
 
+  /**
+   * Slice the Dataframe by a single row
+   *
+   * @param label The row label to slice
+   * @return the row data
+   */
   public row(label: string): Series<T> {
     return new Series(this.index.map((i: string) => this.data[i].at(label)), this.columns());
   }
 
+  /**
+   * Return all row labels
+   */
   public rows(): string[] {
     const distinct: (value: string, index: number, self: string[]) => boolean
       = (value: string, index: number, self: string[]): boolean =>
@@ -130,6 +181,9 @@ class DataFrame<T> {
     return allColumns.filter(distinct);
   }
 
+  /**
+   * Format this dataframe into a string for display
+   */
   public format(): string {
     const toFormat: DataFrame<string> = this.map((v: T): string => JSON.stringify(v));
     const rowNames: string[] = this.rows();
@@ -161,16 +215,34 @@ class DataFrame<T> {
     return `${header}\n${table}`;
   }
 
+  /**
+   * Map a function over all values in this dataframe and return the resulting dataframe
+   *
+   * @param f The function to map
+   * @return A new Dataframe containing mapped values
+   */
   public map<U>(f: (value: T) => U): DataFrame<U> {
     return new DataFrame(this.index.map((col: string): Series<U> => this.data[col].map(f)),
                          this.index);
   }
 
+  /**
+   * Find the maximum value in the dataframe
+   *
+   * @param lens A Ramda Lens focused on the value for which to compute the max
+   * @return The maximum value.
+   */
   public max(lens: R.Lens): R.Ord {
     const maxSeries = R.map((col: string): R.Ord => this.data[col].max(lens), this.index);
     return R.reduce(R.max, maxSeries[0], maxSeries);
   }
 
+  /**
+   * Find the minimum value in the dataframe
+   *
+   * @param lens A Ramda Lens focused on the value for which to compute the min
+   * @return The minimum value.
+   */
   public min(lens: R.Lens): R.Ord {
     const minSeries = R.map((col: string): R.Ord => this.data[col].min(lens), this.index);
     return R.reduce(R.min, minSeries[0], minSeries);
@@ -204,6 +276,12 @@ class DataFrame<T> {
     return R.reduce(reducer, data[0], data) as [string, string, T];
   }
 
+  /**
+   * Normalise all values in the dataframe such that the max() === 1
+   *
+   * @param lens A Ramda Lens focused on the value to normalise
+   * @return A normalised copy of this dataframe
+   */
   public normalise(lens: R.Lens): DataFrame<T> {
     const max: R.Ord = this.max(lens);
 
@@ -214,6 +292,12 @@ class DataFrame<T> {
     }
   }
 
+  /**
+   * Reorder the columns of this dataframe
+   *
+   * @param newIndex Arrange the existing columns to conform to this as the new column labels
+   * @return A copy of this dataframe with `newIndex` as the new order of column labels
+   */
   public reorderColumns(newIndex: string[]): DataFrame<T> {
     const constrainedIndex: string[]
       = newIndex.filter((i: string): boolean => this.index.indexOf(i) > -1);
@@ -222,6 +306,12 @@ class DataFrame<T> {
                          constrainedIndex);
   }
 
+  /**
+   * Reorder the rows of this dataframe
+   *
+   * @param newIndex Arrange the existing rows to conform to this as the new row labels
+   * @return A copy of this dataframe with `newIndex` as the new order of row labels
+   */
   public reorderRows(newIndex: string[]): DataFrame<T> {
     return new DataFrame(
       this.index.map((col: string): Series<T> => this.data[col].reorder(newIndex)),
@@ -246,14 +336,9 @@ class DataFrame<T> {
   }
 }
 
-interface CSVRow {
-  [propName: string]: string;
-}
-
-interface CSV extends Array<CSVRow> {
-  readonly columns: string[];
-}
-
+/**
+ * Convert a D3 parsed CSV into a DataFrame<Node>
+ */
 const fromCSV: (data: DSVRowArray<string>) => DataFrame<Node>
   = (data: DSVRowArray<string>): DataFrame<Node> => {
     const seriesIndexName: string = data.columns[0];
@@ -282,8 +367,11 @@ const fromCSV: (data: DSVRowArray<string>) => DataFrame<Node>
   };
 
 
+/**
+ * Convert a 2D array of T into a DataFrame<T>
+ */
 const fromArray: <T>(data: T[][]) => DataFrame<T>
   = <T>(data: T[][]): DataFrame<T> =>
     new DataFrame<T>(data.map((row: T[]): Series<T> => new Series<T>(row)));
 
-export { ancestorOf, count, countRatio, CSV, fromArray, fromCSV, DataFrame, outerRadialDomain };
+export { ancestorOf, count, countRatio, fromArray, fromCSV, DataFrame, outerRadialDomain };
